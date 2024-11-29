0626e4212216313c1c1c48431a65f78fd2ab05ab
Jan Swidzinski <50329232+Ultr0x@users.noreply.github.com>
2024-11-29 02:26:27 +0100

    Adding text transformers

--- src/transformers.py
+++ src/transformers.py
@@ -1,6 +1,39 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import numpy as np
+import string
+import re
+import emoji
+import nltk
+
+# nltk.download("punkt")
+# nltk.download("wordnet")
+# from nltk.tokenize import word_tokenize
+# from nltk.stem import WordNetLemmatizer
+
+
+class TextTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Flatten X to get a 1D array if it's not already
+        texts = X.flatten()
+        processed_texts = []
+        for text in texts:
+            if not isinstance(text, str):
+                text = ""  # Handle NaN or non-string values
+            text = re.sub(r"@\w+", "", text)
+            text = text.lower()
+            processed_texts.append(text)
+
+        # Return a list of strings
+        return processed_texts
+
 
 class DropColumnTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -11,23 +44,24 @@ class DropColumnTransformer(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         return X.drop(columns=self.columns)
-    
+
+
 class TimeTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
     def get_feature_names_out(self, feature_names_out):
         return [
-            'time__day_of_year_sin', 
-            'time__day_of_year_cos',
-            'time__day_of_week',
-            'time__time_of_day_sin',
-            'time__time_of_day_cos',
-            ]
+            "time__day_of_year_sin",
+            "time__day_of_year_cos",
+            "time__day_of_week",
+            "time__time_of_day_sin",
+            "time__time_of_day_cos",
+        ]
 
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X):
         # X_copy = X.copy()
 
@@ -41,25 +75,45 @@ class TimeTransformer(BaseEstimator, TransformerMixin):
         time_of_day = dt.hour * 60 + dt.minute
 
         day_of_year_sin, day_of_year_cos = get_sin_cos(day_of_year, 366)
-        time_of_day_sin, time_of_day_cos = get_sin_cos(time_of_day, 24*60)
+        time_of_day_sin, time_of_day_cos = get_sin_cos(time_of_day, 24 * 60)
 
-        return pd.DataFrame(data={
-            'day_of_year_sin': day_of_year_sin,
-            'day_of_year_cos': day_of_year_cos,
-            'day_of_week': day_of_week,
-            'time_of_day_sin': time_of_day_sin,
-            'time_of_day_cos': time_of_day_cos,
-        })
-    
+        return pd.DataFrame(
+            data={
+                "day_of_year_sin": day_of_year_sin,
+                "day_of_year_cos": day_of_year_cos,
+                "day_of_week": day_of_week,
+                "time_of_day_sin": time_of_day_sin,
+                "time_of_day_cos": time_of_day_cos,
+            }
+        )
+
+
 class TextTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X):
-        # X is a 2D array (samples, 1) of column `text`
-        # get rid of @airline (pass possible names to init?)
-        # enoce
-        return X
+        texts = X.flatten()
+        processed_texts = []
+        for text in texts:
+            if not isinstance(text, str):
+                text = ""
+            text = re.sub(r"@\w+", "", text)
+            text = text.lower()
+            text = emoji.demojize(text)
+            # Add space around demojized text
+            text = re.sub(r"(:\w+:)", r" \1 ", text)
+            processed_texts.append(text)
+        # Lemmatization
+        #             from nltk.stem import WordNetLemmatizer
+        #             nltk.download('wordnet')
+        #             lemmatizer = WordNetLemmatizer()
+        #             tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        #             Return a list of strings
+
+        # Also we need to delte https:// and http:// links maybe
+
+        return processed_texts
