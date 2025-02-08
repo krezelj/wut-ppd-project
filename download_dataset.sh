677c9b37c889e9739f08796787515c2163441482
michal jan wlodarczyk <mwlodarzc@gmail.com>
2025-02-08 22:42:13 +0100

    feat: add download_dataset.sh script

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -L -o ~/Downloads/archive.zip https://www.kaggle.com/api/v1/datasets/download/crowdflower/twitter-airline-sentiment
+curl -L -o ./data/archive.zip https://www.kaggle.com/api/v1/datasets/download/crowdflower/twitter-airline-sentiment
+unzip ./data/archive.zip -d ./data
+rm ./data/archive.zip
